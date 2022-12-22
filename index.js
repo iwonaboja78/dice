4a29c3dc137dd742f1d4313fceecd221731eccd9
iwonaboja78 <58893354+iwonaboja78@users.noreply.github.com>
2022-12-22 15:37:39 +0100

    Uploading files

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+
+function clickMe() {
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+  var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+
+  var randomImageSource1 = "images/" + randomDiceImage1;
+
+  var image1 = document.querySelectorAll("img")[0];
+
+  image1.setAttribute("src", randomImageSource1);
+
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+  const butn = document.querySelector("button");
+
+  
+    if (randomNumber1 > randomNumber2) {
+      document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!"
+    } else if (randomNumber1 === randomNumber2) {
+      document.querySelector("h1").innerHTML = "Draw!!!"
+    } else {
+      document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!"
+    }
+}
